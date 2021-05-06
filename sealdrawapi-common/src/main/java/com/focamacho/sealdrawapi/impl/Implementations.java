50c6f89b9e5948380f58c2a23971fa06d3308579
Focamacho <renancolombomail@gmail.com>
2021-05-06 18:27:19 -0300

    Atualizar nome das classes

--- sealdrawapi-common/src/main/java/com/focamacho/sealdrawapi/impl/Implementations.java
+++ sealdrawapi-common/src/main/java/com/focamacho/sealdrawapi/impl/Implementations.java
@@ -18,18 +18,18 @@ public class Implementations {
         //Bukkit, Bungee ou Sponge
         try {
             Class.forName("org.bukkit.Bukkit");
-            toAssign = Class.forName("com.focamacho.sealdrawapi.api.BukkitPaint").getDeclaredConstructor(Drawing.class);
-            Class.forName("com.focamacho.sealdrawapi.SealDrawAPIBukkit").getMethod("onEnable", SealDrawAPI.class).invoke(null, instance);
+            toAssign = Class.forName("com.focamacho.sealdrawapi.bukkit.api.Paint").getDeclaredConstructor(Drawing.class);
+            Class.forName("com.focamacho.sealdrawapi.bukkit.SealDrawAPIBukkit").getMethod("onEnable", SealDrawAPI.class).invoke(null, instance);
         } catch (ClassNotFoundException | NoSuchMethodException | IllegalAccessException | InvocationTargetException ignore) {
             try {
                 Class.forName("net.md_5.bungee.api.ProxyServer");
-                toAssign = Class.forName("com.focamacho.sealdrawapi.api.BungeePaint").getDeclaredConstructor(Drawing.class);
-                Class.forName("com.focamacho.sealdrawapi.SealDrawAPIBungee").getMethod("onEnable", SealDrawAPI.class).invoke(null, instance);
+                toAssign = Class.forName("com.focamacho.sealdrawapi.bungee.api.Paint").getDeclaredConstructor(Drawing.class);
+                Class.forName("com.focamacho.sealdrawapi.bungee.SealDrawAPIBungee").getMethod("onEnable", SealDrawAPI.class).invoke(null, instance);
             } catch (ClassNotFoundException | NoSuchMethodException | IllegalAccessException | InvocationTargetException ignored) {
                 try {
                     Class.forName("org.spongepowered.api.Sponge");
-                    toAssign = Class.forName("com.focamacho.sealdrawapi.api.SpongePaint").getDeclaredConstructor(Drawing.class);
-                    Class.forName("com.focamacho.sealdrawapi.SealDrawAPISponge").getMethod("onEnable", SealDrawAPI.class).invoke(null, instance);
+                    toAssign = Class.forName("com.focamacho.sealdrawapi.sponge.api.Paint").getDeclaredConstructor(Drawing.class);
+                    Class.forName("com.focamacho.sealdrawapi.sponge.SealDrawAPISponge").getMethod("onEnable", SealDrawAPI.class).invoke(null, instance);
                 } catch (ClassNotFoundException | NoSuchMethodException | IllegalAccessException | InvocationTargetException ignored1) {}
             }
         }
