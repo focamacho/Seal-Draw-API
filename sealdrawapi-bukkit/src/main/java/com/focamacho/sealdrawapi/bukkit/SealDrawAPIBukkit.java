c0baa7bea95eacb876f176fc488e31a625579f73
Focamacho <renancolombomail@gmail.com>
2021-05-08 12:47:06 -0300

    Injetar valor na config do Spigot para evitar Kick por Spam #13

--- sealdrawapi-bukkit/src/main/java/com/focamacho/sealdrawapi/bukkit/SealDrawAPIBukkit.java
+++ sealdrawapi-bukkit/src/main/java/com/focamacho/sealdrawapi/bukkit/SealDrawAPIBukkit.java
@@ -4,12 +4,16 @@ import com.focamacho.sealdrawapi.SealDrawAPI;
 import com.focamacho.sealdrawapi.bukkit.command.DrawCommand;
 import com.focamacho.sealdrawapi.bukkit.logger.DrawFilter;
 import com.focamacho.sealdrawapi.bukkit.packet.PacketHandler;
+import com.google.common.collect.Lists;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.core.Logger;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.Plugin;
 
-@SuppressWarnings("unused")
+import java.lang.reflect.Field;
+import java.util.List;
+
+@SuppressWarnings({"unused", "unchecked"})
 public final class SealDrawAPIBukkit {
 
     public static void onEnable(SealDrawAPI instance) {
@@ -17,6 +21,16 @@ public final class SealDrawAPIBukkit {
         ((Logger) LogManager.getRootLogger()).addFilter(new DrawFilter());
 
         try { Bukkit.getPluginManager().registerEvents(new PacketHandler(instance), (Plugin) instance.getPlugin()); } catch(Exception e) { e.printStackTrace(); }
+        try {
+            Class<?> configClass = Class.forName("org.spigotmc.SpigotConfig");
+            Field spamField = configClass.getField("spamExclusions");
+            List<String> spamList = (List<String>) spamField.get(null);
+            if(!spamList.contains("/sdwa")) {
+                List<String> toSet = Lists.newArrayList(spamList);
+                toSet.add("/sdwa");
+                spamField.set(null, toSet);
+            }
+        } catch(Exception ignored) {}
     }
 
 }
